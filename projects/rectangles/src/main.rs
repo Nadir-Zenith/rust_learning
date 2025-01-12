8544dfe6c0ccb3e877f67893b2fe305540b395a1
Bijaya Khadka <zen.kalon@gmail.com>
2025-01-12 23:17:45 +0545

    some more methods

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -8,11 +8,16 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height 
     }
-}
 
-impl Rectangle{
     fn can_hold(&self, other: &Rectangle) -> bool {
         (self.width > other.width) && (self.height > other.height)
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
