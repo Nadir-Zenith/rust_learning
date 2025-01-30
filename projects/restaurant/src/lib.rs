c7cf336896fb5f70553924fe60996269829b4780
Bijaya Khadka <zen.kalon@gmail.com>
2025-01-30 23:04:04 +0545

    using use keyword

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -45,10 +45,11 @@ mod back_of_house {
     fn cook_order() {}
 }
 
+pub use crate::front_of_house::hosting
 
 pub fn eat_at_restaurante() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
