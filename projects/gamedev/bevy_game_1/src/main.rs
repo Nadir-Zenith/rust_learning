adcc36ec4bd159249dc2836bff2be435ed899838
Bijaya Khadka <zen.kalon@gmail.com>
2025-06-03 12:34:32 +0545

    map and assets setup

--- projects/gamedev/bevy_game_1/src/main.rs
+++ projects/gamedev/bevy_game_1/src/main.rs
@@ -3,5 +3,31 @@ use bevy::prelude::*;
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, (spawn_camera, spawn_map));
     app.run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3d::default());
+}
+
+fn spawn_map(
+    mut commands: Commands,
+    mut mesh_assets: Res<Assets<Mesh>>,
+    mut material_assets: Res<Assets<StandardMaterial>>,
+) {
+    commands.spawn(DirectionalLight::default());
+    let ball_mesh = mesh_assets.add(Sphere::new(1.));
+    for h in 0..16 {
+        let color = Color::hsl((h as f32 / 16.) * 360., 1., 0.5);
+        let ball_material = material_assets.add(StandardMaterial {
+            base_color: color,
+            ..Default::default()
+        });
+        commands.spawn((
+            Transform::from_translation(Vec3::new((-8. + h as f32) * 2., 0., -50.0)),
+            Mesh3d(ball_mesh.clone()),
+            MeshMaterial3d(ball_material),
+        ));
+    }
 }
