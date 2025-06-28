a52de77b910e1ab71353b80933f559d468009083
Bijaya Khadka <zen.kalon@gmail.com>
2025-06-28 20:19:34 +0545

    json response with serde

--- projects/hello_rust_api/hello_main/src/main.rs
+++ projects/hello_rust_api/hello_main/src/main.rs
@@ -1,14 +1,25 @@
+use axum::http::StatusCode;
 use axum::routing::get;
-use axum::Router;
+use axum::{Json, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/", get(hello_json));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn hello() -> &'static str {
-    "Hello, world!!"
+async fn hello_json() -> (StatusCode, Json<Response>) {
+    let response = Response {
+        message: "Hello, world!",
+    };
+
+    (StatusCode::OK, Json(response))
 }
