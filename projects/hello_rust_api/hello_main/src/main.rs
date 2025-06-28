8322f55c1b087ad477faafb774e583c5f90b3c53
Bijaya Khadka <zen.kalon@gmail.com>
2025-06-28 20:08:41 +0545

    axum server

--- projects/hello_rust_api/hello_main/src/main.rs
+++ projects/hello_rust_api/hello_main/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, world!!"
 }
