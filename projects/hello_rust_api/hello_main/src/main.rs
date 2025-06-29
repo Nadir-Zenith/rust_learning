07d0a545312b674d7b60cba3fe4106e065effd53
Bijaya Khadka <zen.kalon@gmail.com>
2025-06-29 20:01:09 +0545

    from and into

--- projects/hello_rust_api/hello_main/src/main.rs
+++ projects/hello_rust_api/hello_main/src/main.rs
@@ -1,17 +1,36 @@
 use anyhow::Context;
 use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Json, Router};
+use rand;
 use serde::Serialize;
+use tower_http;
 
 #[derive(Serialize)]
 struct Response {
     message: &'static str,
 }
 
+struct AppError(anyhow::Error);
+
+impl From<anyhow::Error> for AppError {
+    fn from(value: anyhow::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new().route("/", get(hello_json));
+    let app = Router::new()
+        .route("/", get(hello_json))
+        .layer(tower_http::catch_panic::CatchPanicLayer::new()); //added
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
@@ -23,10 +42,18 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn hello_json() -> (StatusCode, Json<Response>) {
+async fn hello_json() -> Result<(StatusCode, Json<Response>), AppError> {
     let response = Response {
-        message: "Hello, world!",
+        message: generate_message().expect("failed to generate message")?,
     };
 
     (StatusCode::OK, Json(response))
+}
+
+/// Generate the hello world message
+fn generate_message() -> anyhow::Result<&'static str> {
+    if rand::random() {
+        anyhow::bail!("no message for you");
+    }
+    Ok("Hello, world!")
 }
