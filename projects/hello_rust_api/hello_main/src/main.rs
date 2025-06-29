620e92645f307485987ebd2ce0ef5cad87c2ad04
Bijaya Khadka <zen.kalon@gmail.com>
2025-06-29 19:33:52 +0545

    error handling using anyhow

--- projects/hello_rust_api/hello_main/src/main.rs
+++ projects/hello_rust_api/hello_main/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use axum::http::StatusCode;
 use axum::routing::get;
 use axum::{Json, Router};
@@ -9,11 +10,17 @@ struct Response {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let app = Router::new().route("/", get(hello_json));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("failed to bind TCP listener")?;
+    axum::serve(listener, app)
+        .await
+        .context("axum::server failed")?;
+
+    Ok(())
 }
 
 async fn hello_json() -> (StatusCode, Json<Response>) {
