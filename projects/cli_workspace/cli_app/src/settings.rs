bf20023d78352746391cf2a682ca928a75897d99
Bijaya Khadka <zen.kalon@gmail.com>
2025-07-07 08:54:42 +0545

    default values

--- projects/cli_workspace/cli_app/src/settings.rs
+++ projects/cli_workspace/cli_app/src/settings.rs
@@ -3,18 +3,20 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize, Default)]
 #[allow(unused)]
 pub struct Database {
-    pub url: String,
+    pub url: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Default)]
 #[allow(unused)]
 pub struct Logging {
-    pub log_level: String,
+    pub log_level: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Default)]
 #[allow(unused)]
 pub struct Settings {
+    #[serde(default)]
     pub database: Database,
+    #[serde(default)]
     pub logging: Logging,
 }
